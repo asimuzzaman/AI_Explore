f8e6fe692b3ee3e8fef93a742d63da061860ffd6
asimuzzaman <mansib.raj@gmail.com>
2019-02-26 20:37:46 +0600

    Add Path class source

---
+++ DijkstraPathFinder/Path.java
@@ -0,0 +1,13 @@
+//package dijkstra;
+
+public class Path {
+    String from;
+    String to;
+    int weight;
+
+    public Path () {
+        from = null;
+        to = null;
+        weight = 0;
+    }
+}
